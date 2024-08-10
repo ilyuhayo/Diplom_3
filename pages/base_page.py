4e75acec029ef4ddac6dc798adb0b88c112bd0f8
Ilya Zolotov <zolotovil@mail.ru>
2024-08-10 15:07:50 +0300

    Create base_page

---
+++ pages/base_page.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+class BasePage:
+    def __int__(self, driver):
+        self.driver = driver
+
+    def go_to_site(self, base_url):
+        return self.driver.get(base_url)
+
+    def find_element_located(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(expected_conditions.presence_of_element_located(locator),
+                                                      message=f'Not found {locator}')
